60c4ffa4fb755975f22060d643b1a8473e430034
dyturralde <darylyturraldelopez@gmail.com>
2025-10-01 15:34:20 -0300

    [FIX] Arreglando error _change_payable_receivable_account

--- pc_account_currency/models/account_move.py
+++ pc_account_currency/models/account_move.py
@@ -6,17 +6,18 @@ class AccountMove(models.Model):
 
     @api.onchange('date', 'currency_id', 'journal_id', 'partner_id', 'invoice_line_ids')
     def _change_payable_receivable_account(self):
-        account_id = self.journal_id.account_currency_ids.filtered(lambda l: l.currency_id == self.currency_id).account_id
-        currency_account_receivable = self.partner_id.property_account_receivable_id.currency_id.id if self.partner_id.property_account_receivable_id.currency_id else self.env.company.currency_id.id
-        if self.move_type in ['out_refund', 'out_invoice'] and self.currency_id.id == currency_account_receivable:
-            account_id = self.partner_id.property_account_receivable_id
-        currency_account_payable = self.partner_id.property_account_payable_id.currency_id.id if self.partner_id.property_account_payable_id.currency_id else self.env.company.currency_id.id
-        if self.move_type in ['in_refund', 'in_invoice'] and self.currency_id.id == currency_account_payable:
-            account_id = self.partner_id.property_account_payable_id
+        for rec in self:
+            account_id = rec.journal_id.account_currency_ids.filtered(lambda l: l.currency_id == rec.currency_id).account_id
+            currency_account_receivable = rec.partner_id.property_account_receivable_id.currency_id.id if rec.partner_id.property_account_receivable_id.currency_id else rec.env.company.currency_id.id
+            if rec.move_type in ['out_refund','out_invoice'] and rec.currency_id and rec.currency_id.id == currency_account_receivable:
+                account_id = rec.partner_id.property_account_receivable_id
+            currency_account_payable = rec.partner_id.property_account_payable_id.currency_id.id if rec.partner_id.property_account_payable_id.currency_id else rec.env.company.currency_id.id
+            if rec.move_type in ['in_refund','in_invoice'] and rec.currency_id and rec.currency_id.id == currency_account_payable:
+                account_id = rec.partner_id.property_account_payable_id
 
-        if account_id and self.journal_id.type != 'general':
-            for line in self.line_ids.filtered(lambda l: l.account_id.account_type in ('asset_receivable', 'liability_payable')):
-                line.account_id = account_id
+            if account_id and rec.journal_id.type != 'general':
+                for line in rec.line_ids.filtered(lambda l: l.account_id.account_type in ('asset_receivable', 'liability_payable')):
+                    line.account_id = account_id
 
     def action_post(self):
         """ Before validate invoice change account for line with account_type in ('asset_receivable', 'liability_payable')"""
