2aad35a933c74cc751c9d0a1ac587bad587a413d
Andres Iglesias <102163345+iglesias-andres@users.noreply.github.com>
2025-12-26 16:07:03 -0300

    Delete stock_importaciones directory

--- stock_importaciones/models/stock_picking_type.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class StockPickingType(models.Model):
-    _inherit = "stock.picking.type"
-    is_import_op = fields.Boolean(string='Op. de Importación')
