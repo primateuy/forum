a9c6928c55a9e73ab9965164967aab67db667204
dyturralde <darylyturraldelopez@gmail.com>
2026-01-29 15:53:55 -0300

    [IMP] Agregandole autor al modulo de franquicia

--- pos_sale_order_sync/__manifest__.py
+++ pos_sale_order_sync/__manifest__.py
@@ -2,6 +2,7 @@
     "name": "POS Sale Order Sync",
     "version": "17.0.0.0",
     "category": "Point of Sale",
+    "author": "PrimateUY",
     "summary": "Create Sale Orders from POS Orders",
     "depends": ["point_of_sale", "sale", 'purchase', 'stock', 'sale_order_type', 'sale_purchase_inter_company_rules', 'l10n_uy_einvoice_base', 'account'],
     "data": [
