d2efc6df11448ad071dffc470e3bb8a65ba6b7ab
primateuy <diego@primate.uy>
2026-04-14 14:03:30 -0300

    modulo errado

--- pos_sale_order_sync - copia/models/dgi_punto_emision.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class DgiPuntoEmision(models.Model):
-    _inherit = "dgi.punto.emision"
-
-    max_numero_lineas = fields.Integer('Cantidad máxima de líneas', default=199)
--- pos_sale_order_sync - copia/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    excluir_logica_franquicia = fields.Boolean('Excluir lógica franquicia', default=False)
